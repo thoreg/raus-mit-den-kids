9d2c327f257a44a8b30636c8d8ae87e8cb224d18
Thomas Rega <thomas.rega@momox.biz>
2014-11-03 13:01:05 +0100

    First simple index page

--- rmdk/rmdk/settings.py
+++ rmdk/rmdk/settings.py
@@ -1,22 +1,5 @@
-"""
-Django settings for rmdk project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
-
-
-
-# Application definition
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -43,35 +26,19 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'rmdk.urls'
-
 WSGI_APPLICATION = 'rmdk.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, "templates"),
+)
--- rmdk/rmdk/urls.py
+++ rmdk/rmdk/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'rmdk.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns('',
+    url(r'^addi/', include(admin.site.urls)),
+    url(r'^$', 'rmdk.location.views.index', name='index'),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
